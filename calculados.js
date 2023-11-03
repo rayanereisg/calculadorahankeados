30262b41963d3158a838de293b1bae54c9219c6f
rayane <92309011+rayanereisg@users.noreply.github.com>
2023-11-03 12:57:20 -0300

    Add files via upload

---
+++ calculados.js
@@ -0,0 +1,31 @@
+function calcularCategoria(vitorias, derrotas) {
+    const saldo = vitorias - derrotas;
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias >= 11 && vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias >= 21 && vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias >= 51 && vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias >= 81 && vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias >= 91 && vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    const mensagem = `O Herói tem um saldo de ${saldo} está no nível de ${nivel}`;
+    return mensagem;
+}
+
+// Exemplo de uso:
+const vitorias = 70;
+const derrotas = 30;
+
+const mensagem = calcularCategoria(vitorias, derrotas);
+
+console.log(mensagem);
